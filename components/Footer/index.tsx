36d634f58b3d1291b38b2b724d842474d39f8222
SammyChris <ebusameric@gmail.com>
2023-02-22 21:03:09 +0100

    add footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.footerContent}>
+        <div>
+          <Link href="/">
+            <h2>Blazedhemps</h2>
+          </Link>
+
+          <ul>
+            <li>Twitter</li>
+            <li>Tiktok</li>
+            <li>Instagram</li>
+            <li>Youtube</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Costumer Service</h3>
+          <ul>
+            <li>Contact Us</li>
+            <li>Live Chat</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Terms of Use</h3>
+          <ul>
+            <li>Privacy Policy</li>
+            <li>Terms of Use</li>
+            <li>Cookie Policy</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Company</h3>
+          <ul>
+            <li>About Us</li>
+            <li>Ethical Journalism Policy</li>
+            <li>Cookie Policy</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.copy}>
+        © 2023 BLAZEDHEMPS, LLC. ALL RIGHTS RESERVED
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
